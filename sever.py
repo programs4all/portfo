11016b6d9413d8506221f5eecd5dc746edf66aca
programs4all <lukesbusiness01@gmail.com>
2021-01-07 19:00:44 +1100

    backdating python and flask versions

--- sever.py
+++ sever.py
@@ -2,34 +2,29 @@ from flask import Flask, render_template, url_for, request, redirect
 import csv
 app = Flask(__name__)
 
+
 @app.route('/')
 def root():
 	return render_template('index.html')
 
+
 @app.route('/<string:page_name>')
 def html_page(page_name):
 	return render_template(page_name)
+
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
 	if request.method == 'POST':
 		try:
 			data = request.form.to_dict()
-			write_to_file(data)
 			write_to_csv(data)
-			return redirect('/ThankYou.html')
+			return redirect('/thankyou.html')
 		except:
 			print('did not write to database.')
 	else:
 		return 'something went wrong'
 
-# email,subject,message
-def write_to_file(data):
-	with open('database.txt', 'a') as db:
-		email = data['email']
-		subject = data['subject']
-		message = data['message']
-		file = db.write(f'\n{email},{subject},{message}')
 
 def write_to_csv(data):
 	with open('database.csv', 'a', newline='') as db2:
@@ -64,6 +59,7 @@ def write_to_csv(data):
 # @app.route('/work.html')
 # def extra_work():
 # 	return render_template('work.html')
+
 # below text negates all above text.
 # @app.route('/<string:page_name>')
 # def html_page(page_name):
